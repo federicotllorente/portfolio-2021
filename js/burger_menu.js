9ebd2a076fef8f1914c63117eedbcb3e1faad708
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-02-09 19:13:40 +0100

    'Download Resume' button added

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -1,10 +1,3 @@
-let burger_menu = document.getElementById("burger_menu");
-let burger_menu_btn = document.getElementById("burger_menu_btn");
-let close_menu_btn = document.getElementById("burger_menu_btn__close_btn");
-
-let showingClass = "burger_menu--showing";
-let menuClassList = burger_menu.classList;
-
 let openAndCloseBurgerMenu = function() {
     let alreadyOpened = menuClassList.contains(showingClass);
     if(!alreadyOpened) {
@@ -12,4 +5,21 @@ let openAndCloseBurgerMenu = function() {
     } else {
         menuClassList.remove(showingClass);
     }
-}
+};
+
+let modifyDownloadResumeBTN = function(mediaQuery, htmlToChange) {
+    if(mediaQuery.matches) {
+        htmlToChange.innerHTML = "Download my<br>Resume/CV";
+    }
+};
+
+let burger_menu = document.getElementById("burger_menu");
+let burger_menu_btn = document.getElementById("burger_menu_btn");
+let close_menu_btn = document.getElementById("burger_menu_btn__close_btn");
+
+let showingClass = "burger_menu--showing";
+let menuClassList = burger_menu.classList;
+
+let download_resume_btn = document.getElementById("download_resume_btn");
+let btnMediaQuery = window.matchMedia("(max-width: 400px)");
+modifyDownloadResumeBTN(btnMediaQuery, download_resume_btn);
